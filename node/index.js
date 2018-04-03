686339f910c62ed21a6dcd610960dfc334416f8b
Nattakit Boonyang <nattakit.boonyang@gmail.com>
2018-04-03 23:21:57 +0700

    :hammer: Rewrite code

--- node/index.js
+++ node/index.js
@@ -4,16 +4,30 @@ const router = express.Router()
 const bodyParser = require('body-parser')
 
 const bears = [
-  { id: 1, name: 'Pooh', weight: 100 },
-  { id: 2, name: 'Winnie', weight: 200 }
+  {
+    id: 1,
+    name: 'Pooh',
+    weight: 100
+  },
+  {
+    id: 2,
+    name: 'Winnie',
+    weight: 200
+  }
 ]
 
 app.use(express.static('public'))
 
-app.use('/api',
-  bodyParser.urlencoded({ extended: false }), router)
+app.use(
+  '/api',
+  bodyParser.urlencoded({
+    extended: false
+  }),
+  router
+)
 
-router.route('/bears')
+router
+  .route('/bears')
   .get((req, res) => res.send(bears))
 
   .post((req, res) => {
@@ -25,14 +39,13 @@ router.route('/bears')
     res.send(bears)
   })
 
-router.route('/bears/:id')
+router
+  .route('/bears/:id')
   .get((req, res) => res.send(bears[req.params.id]))
-
   .delete((req, res) => {
     delete bears[req.params.id]
     res.send(bears)
   })
-
   .put((req, res) => {
     const id = req.params.id
     bears[id] = {
