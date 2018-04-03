14bd03f17ceffe6e2573edf5a3f344ff8b68cbe3
Nattakit Boonyang <nattakit.boonyang@gmail.com>
2018-04-03 10:12:18 +0700

    Rewrite syntax

--- node/index.js
+++ node/index.js
@@ -3,39 +3,41 @@ const app = express()
 const router = express.Router()
 const bodyParser = require('body-parser')
 
-let bears = [ { id:1, name:'Pooh', weight:100},
-	{ id:2, name:'Winnie', weight:200}, ]
+let bears = [
+  { id: 1, name: 'Pooh', weight: 100 },
+  { id: 2, name: 'Winnie', weight: 200 }
+]
 
 app.use(express.static('public'))
 
 app.use('/api',
-		bodyParser.urlencoded({extended:false}), router)
+  bodyParser.urlencoded({ extended: false }), router)
 
 router.route('/bears')
-	.get((req,res) => res.send(bears))
+  .get((req, res) => res.send(bears))
 
-	.post((req,res) => {
-		let bear = {}
-		bear.id = bears.length+1
-		bear.name = req.body.name
-		bear.weight = req.body.weight
-		bears.push(bear)
-		res.send(bears)
-	})
+  .post((req, res) => {
+    let bear = {}
+    bear.id = bears.length + 1
+    bear.name = req.body.name
+    bear.weight = req.body.weight
+    bears.push(bear)
+    res.send(bears)
+  })
 
 router.route('/bears/:id')
-	.get( (req,res) => res.send(bears[req.params.id]))
+  .get((req, res) => res.send(bears[req.params.id]))
 
-	.delete( (req,res) => {
-		delete bears[ req.params.id ]
-		res.send(bears)
-	})
+  .delete((req, res) => {
+    delete bears[req.params.id]
+    res.send(bears)
+  })
 
-	.put( (req,res) => {
-		const id = req.params.id		
-		bears[id].name = req.body.name
-		bears[id].weight = req.body.weight		
-		res.send(bears)
-	})
+  .put((req, res) => {
+    const id = req.params.id
+    bears[id].name = req.body.name
+    bears[id].weight = req.body.weight
+    res.send(bears)
+  })
 
 app.listen(80)
