c4b4d7f50d19069e1295bdb1f66065713ee76f7b
Nattakit Boonyang <nattakit.boonyang@gmail.com>
2018-04-05 00:14:09 +0700

    :hammer: Fix style

--- node/index.js
+++ node/index.js
@@ -1,59 +1,45 @@
 const express = require('express')
+const bodyParser = require('body-parser')
+const bears = require('./mockup-data')
 const app = express()
 const router = express.Router()
-const bodyParser = require('body-parser')
+const PORT = process.env.PORT || 3000
 
-const bears = [
-  {
-    id: 1,
-    name: 'Pooh',
-    weight: 100
-  },
-  {
-    id: 2,
-    name: 'Winnie',
-    weight: 200
-  }
-]
-
-app.use(express.static('public'))
-
-app.use(
-  '/api',
-  bodyParser.urlencoded({
-    extended: false
-  }),
-  router
-)
+app
+  .use(express.static('public'))
+  .use(
+    '/api',
+    bodyParser.urlencoded({
+      extended: false
+    }),
+    router
+  )
+  .listen(PORT, () => console.log(`listen on ${PORT}`))
 
 router
   .route('/bears')
   .get((req, res) => res.send(bears))
-
-  .post((req, res) => {
+  .post(({ body: { name, weight } }, res) => {
     bears.push({
       id: bears.length + 1,
-      name: req.body.name,
-      weight: req.body.weight
+      name: name,
+      weight: weight
     })
     res.send(bears)
   })
 
 router
   .route('/bears/:id')
-  .get((req, res) => res.send(bears[req.params.id]))
+  .get(({ params: { id } }, res) => res.send(bears[id]))
   .delete((req, res) => {
     delete bears[req.params.id]
     res.send(bears)
   })
-  .put((req, res) => {
-    const id = req.params.id
+  .put(({ params: { id }, body: { name, weight } }, res) => {
     bears[id] = {
       id: id,
-      name: req.body.name,
-      weight: req.body.weight
+      name: name,
+      weight: weight
     }
     res.send(bears)
   })
-
-app.listen(3000)
